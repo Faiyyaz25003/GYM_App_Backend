b749b6e3ca5d568a73a513f31cc021c6733bdd72
Faiyyaz25003 <khanwork25003@gmail.com>
2026-02-24 16:17:08 +0530

    sd

--- Routes/progressRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { analyzeProgress, getAllProgress } from "../Controller/progressController.js";
-
-const router = express.Router();
-
-router.post("/analyze", analyzeProgress);
-router.get("/", getAllProgress);
-
-export default router;
--- Server.js
+++ Server.js
@@ -2,10 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import progressRoutes from "./Routes/progressRoutes.js";
-import superAdminRoutes from "./Routes/superAdminRoutes.js";
-import adminRoutes from "./Routes/adminRoutes.js";
-import trainerRoutes from "./Routes/trainerRoutes.js";
 
 dotenv.config();
 
@@ -13,22 +9,16 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
 
-// Routes
-app.use("/api/progress", progressRoutes);
-app.use("/api/superadmin", superAdminRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/trainers", trainerRoutes);
 
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.error("MongoDB connection error:", err));
 
+// Start server
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT , "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
